6554bc3dd0da5d3cd445d13801901a2cb52a4c46
phanupongr <phanupong.ra@gmail.com>
2025-05-27 13:21:44 +0700

    fix

--- environments/nonprod/compute-demo/main.tf
+++ environments/nonprod/compute-demo/main.tf
@@ -14,22 +14,22 @@ resource "local_file" "default" {
   EOT
 }
 
-# resource "aws_security_group" "unsecure_sg" {
-#   name        = "allow_all"
-#   description = "Allow all inbound traffic (not secure)"
-#   vpc_id      = "vpc-12345678"
+resource "aws_security_group" "unsecure_sg" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic (not secure)"
+  vpc_id      = "vpc-12345678"
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
